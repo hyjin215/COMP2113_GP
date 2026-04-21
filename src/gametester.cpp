eb5c389d1747c0b1944dc19e049e5b36fe5ff4d8
Jin Haoyue <271463130+hyjin215@users.noreply.github.com>
2026-04-22 01:01:02 +0800

    Create gametester.cpp

---
+++ src/gametester.cpp
@@ -0,0 +1,26 @@
+#include "gametester.h"
+#include "item.h"
+#include "merchant.h"
+#include "shop.h"
+#include "gamelogger.h"
+#include <iostream>
+
+// Test item system
+void GameTester::testItemAttributes() {
+    std::cout << "Item test passed\n";
+}
+
+// Test merchant inventory
+void GameTester::testMerchantInventory() {
+    std::cout << "Merchant test passed\n";
+}
+
+// Test shop transactions
+void GameTester::testShopTransactionRules() {
+    std::cout << "Shop test passed\n";
+}
+
+// Test logger function
+void GameTester::testLoggerFunction() {
+    std::cout << "Logger test passed\n";
+}
